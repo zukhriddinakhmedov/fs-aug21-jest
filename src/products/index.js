596f950a5bcccdcf7063683e018e0b284652357d
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2021-12-07 15:31:22 +0100

    Finished CRUD for products

--- src/products/index.js
+++ src/products/index.js
@@ -8,10 +8,37 @@ productsRouter
         const products = await ProductModel.find({})
         res.send(products)
     })
+    .get('/:id', async (req, res) => {
+        const product = await ProductModel.findById(req.params.id)
+
+        console.log(product)
+        if (!product) {
+            res.status(404).send()
+        } else {
+            res.send(product)
+        }
+    })
     .post("/", async (req, res) => {
         const product = new ProductModel(req.body)
         await product.save()
         res.status(201).send(product)
+    })
+    .put("/:id", async (req, res) => {
+        const product = await ProductModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+        if (!product) {
+            res.status(404).send()
+        } else {
+            res.send(product)
+        }
+    })
+    .delete("/:id", async (req, res) => {
+        const product = await ProductModel.findByIdAndDelete(req.params.id)
+        if (!product) {
+            res.status(404).send()
+        } else {
+            res.status(204).send()
+        }
     })
 
 export default productsRouter
